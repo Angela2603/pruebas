8102c38e7fd7a15eaecd85979d9aaaf190eb6d74
Angela2603 <a20180405@pucp.edu.pe>
2024-06-25 12:17:31 -0500

    Update prueba 1.py

--- prueba 1.py
+++ prueba 1.py
@@ -1,8 +1,11 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-
-# Definir el CSS como una cadena de texto
-css = """
+# Definir el HTML y CSS directamente en una cadena de texto
+html_code = """
+<!DOCTYPE html>
+<html>
+<head>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Open+Sans:wght@400;700&display=swap');
 
@@ -29,6 +32,17 @@ body {
     color: #444;
 }
 </style>
+</head>
+<body>
+<h1 class="title">Título con Open Sans</h1>
+<h2 class="subtitle">Subtítulo con Roboto</h2>
+<p class="custom-text">Este es un párrafo con Open Sans.</p>
+<p>Este es un párrafo con la tipografía predeterminada (Roboto).</p>
+</body>
+</html>
 """
+
+# Renderizar el componente HTML en Streamlit
+components.html(html_code, height=600)
 
 st.header("Este es un header")
